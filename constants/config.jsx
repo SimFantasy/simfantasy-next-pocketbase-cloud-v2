1e1e3086341e698b934639dfe4bfa33e2e094669
Simz <simbox@qq.com>
2023-03-02 00:04:31 +0800

    remove some not used package

--- constants/config.jsx
+++ constants/config.jsx
@@ -4,9 +4,6 @@ export const siteName = 'Sim.z'
 export const authorId = process.env.NEXT_PUBLIC_AUTHOR_ID
 export const pocketbaseApiUrl = process.env.NEXT_PUBLIC_POCKETBASE_API_URL
 
-export const pb = new PocketBase(pocketbaseApiUrl)
-export const isValid = pb.authStore.isValid
-
 export const featureLimit = 5
 export const pageLimit = 3
 
