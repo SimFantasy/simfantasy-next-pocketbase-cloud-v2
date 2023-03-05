a03a848023f04f45d27fc2ab1c221b1c37b94805
Simz <simbox@qq.com>
2023-03-05 15:50:52 +0800

    add image server host

--- next.config.js
+++ next.config.js
@@ -1,28 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8090',
-        pathname: '/api/files/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.sspai.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'fantasy.pockethost.io',
-      },
-      {
-        protocol: 'https',
-        hostname: 'simfantasy.github.io',
-      },
-    ],
-  },
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'http',
+				hostname: 'localhost',
+				port: '8090',
+				pathname: '/api/files/**',
+			},
+			{
+				protocol: 'https',
+				hostname: '**.sspai.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'fantasy.pockethost.io',
+			},
+			{
+				protocol: 'https',
+				hostname: 'simfantasy.github.io',
+			},
+			{
+				protocol: 'https',
+				hostname: '**.sda1.dev',
+			},
+		],
+	},
 }
 
 module.exports = nextConfig
