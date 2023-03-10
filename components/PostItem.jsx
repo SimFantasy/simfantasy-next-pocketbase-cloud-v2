7ce603327fed71e5508239914c11aa59c745a759
Simz <simbox@qq.com>
2023-03-10 22:44:54 +0800

    change next image display, edited to html tag

--- components/PostItem.jsx
+++ components/PostItem.jsx
@@ -1,64 +1,60 @@
 import Link from 'next/link'
-import Image from 'next/image'
 import { RiCalendar2Line, RiFolder3Line } from 'react-icons/ri'
 import { dateNowFormat } from '@/utils'
 
 const PostItem = ({
-  title,
-  id,
-  slug,
-  created,
-  description,
-  showCoverImage,
-  coverImage,
-  expand: { category },
+	title,
+	id,
+	slug,
+	created,
+	description,
+	showCoverImage,
+	coverImage,
+	expand: { category },
 }) => {
-  return (
-    <div className='p-6 bg-white rounded flex flex-col dark:bg-gray-900 hover:shadow-lg dark:hover:shadow-none'>
-      <div className='flex flex-col md:flex-row justify-between items-center gap-6'>
-        {coverImage && showCoverImage && (
-          <Link
-            href={`/post/${id}/${slug}`}
-            className='max-w-[320px] max-h-[180px] overflow-hidden rounded-md'
-          >
-            <Image
-              src={coverImage}
-              width={480}
-              height={320}
-              alt={title}
-              className='w-full md:w-auto h-auto rounded-md object-cover object-center'
-              priority
-            />
-          </Link>
-        )}
-        <div className='flex-1 flex flex-col'>
-          <Link
-            href={`/post/${id}/${slug}`}
-            className=' flex justify-start h-full text-xl text-gray-800 line-clamp-2 hover:text-gray-500 dark:text-gray-100'
-          >
-            {title}
-          </Link>
-          <div className='flex justify-start items-center gap-6 my-2 text-sm text-gray-400'>
-            <div className='flex justify-start items-center gap-2 h-full'>
-              <RiCalendar2Line /> {dateNowFormat(created)}
-            </div>
-            <div className='flex justify-start items-center gap-2 h-full'>
-              <RiFolder3Line />
-              <Link
-                href={`/post/category/${category.id}/${category.slug}`}
-                className='hover:text-gray-800'
-              >
-                {category.name}
-              </Link>
-            </div>
-          </div>
-          <article className='text-md text-gray-500 leading-8 line-clamp-3'>
-            {description}
-          </article>
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<div className='p-6 bg-white rounded flex flex-col dark:bg-gray-900 hover:shadow-lg dark:hover:shadow-none'>
+			<div className='flex flex-col md:flex-row justify-between items-center gap-6'>
+				{coverImage && showCoverImage && (
+					<Link
+						href={`/post/${id}/${slug}`}
+						className='max-w-[320px] max-h-[180px] overflow-hidden rounded-md'
+					>
+						<img
+							src={coverImage}
+							alt={title}
+							className='w-full md:w-auto h-auto rounded-md object-cover object-center'
+						/>
+					</Link>
+				)}
+				<div className='flex-1 flex flex-col'>
+					<Link
+						href={`/post/${id}/${slug}`}
+						className=' flex justify-start h-full text-xl text-gray-800 line-clamp-2 hover:text-gray-500 dark:text-gray-100'
+					>
+						{title}
+					</Link>
+					<div className='flex justify-start items-center gap-6 my-2 text-sm text-gray-400'>
+						<div className='flex justify-start items-center gap-2 h-full'>
+							<RiCalendar2Line /> {dateNowFormat(created)}
+						</div>
+						<div className='flex justify-start items-center gap-2 h-full'>
+							<RiFolder3Line />
+							<Link
+								href={`/post/category/${category.id}/${category.slug}`}
+								className='hover:text-gray-800'
+							>
+								{category.name}
+							</Link>
+						</div>
+					</div>
+					<article className='text-md text-gray-500 leading-8 line-clamp-3'>
+						{description}
+					</article>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default PostItem
--- pages/post/[...param].jsx
+++ pages/post/[...param].jsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import Image from 'next/image'
 import Head from 'next/head'
 import { RiCalendar2Line, RiFolder3Line } from 'react-icons/ri'
 import { fetchPosts, fetchPost } from '@/service/apis'
@@ -8,89 +7,86 @@ import { fullDateFormat, serializationString } from '@/utils'
 import { siteName } from '@/constants/config'
 
 const PostPage = ({ post }) => {
-  const {
-    title,
-    coverImage,
-    created,
-    content,
-    tags,
-    expand: { category },
-  } = post
-  return (
-    <>
-      <Head>
-        <title>
-          {title} - {category.name} | {siteName}
-        </title>
-      </Head>
-      <div className='page-main flex flex-col'>
-        {coverImage && (
-          <div className='w-full max-h-[320px] rounded-md overflow-hidden mb-10'>
-            <Image
-              src={coverImage}
-              alt={title}
-              width={768}
-              height={430}
-              priority
-              className='w-full h-auto object-contain object-center rounded-md'
-            />
-          </div>
-        )}
-        <h2 className='text-xl text-gray-800 dark:text-white font-semibold'>
-          {title}
-        </h2>
-        <div className='pt-3 pb-4 flex justify-start items-center gap-6 text-gray-400 text-sm'>
-          <div className='flex justify-start items-center gap-2 h-full'>
-            <RiCalendar2Line /> {fullDateFormat(created)}
-          </div>
-          <div className='flex justify-start items-center gap-2 h-full'>
-            <RiFolder3Line />{' '}
-            <Link
-              href={`/post/category/${category.id}/${category.slug}`}
-              className='hover:text-gray-800'
-            >
-              {category.name}
-            </Link>
-          </div>
-        </div>
-        <article className='text-lg leading-8'>
-          <MarkdownContent source={content} />
-        </article>
-        <div className='divider-line'></div>
-        <div className='flex justify-start items-center gap-4 h-10 mt-6 text-gray-500'>
-          {serializationString(tags).map((tag, index) => (
-            <Link
-              href={`/search?keywords=${tag}`}
-              key={index}
-              className='px-2 py-1 bg-gray-300 rounded text-sm dark:bg-gray-900 dark:text-gray-300'
-            >
-              {tag}
-            </Link>
-          ))}
-        </div>
-      </div>
-    </>
-  )
+	const {
+		title,
+		coverImage,
+		created,
+		content,
+		tags,
+		expand: { category },
+	} = post
+	return (
+		<>
+			<Head>
+				<title>
+					{title} - {category.name} | {siteName}
+				</title>
+			</Head>
+			<div className='page-main flex flex-col'>
+				{coverImage && (
+					<div className='w-full max-h-[320px] rounded-md overflow-hidden mb-10'>
+						<img
+							src={coverImage}
+							alt={title}
+							className='w-full h-auto object-contain object-center rounded-md'
+						/>
+					</div>
+				)}
+				<h2 className='text-xl text-gray-800 dark:text-white font-semibold'>
+					{title}
+				</h2>
+				<div className='pt-3 pb-4 flex justify-start items-center gap-6 text-gray-400 text-sm'>
+					<div className='flex justify-start items-center gap-2 h-full'>
+						<RiCalendar2Line /> {fullDateFormat(created)}
+					</div>
+					<div className='flex justify-start items-center gap-2 h-full'>
+						<RiFolder3Line />{' '}
+						<Link
+							href={`/post/category/${category.id}/${category.slug}`}
+							className='hover:text-gray-800'
+						>
+							{category.name}
+						</Link>
+					</div>
+				</div>
+				<article className='text-lg leading-8'>
+					<MarkdownContent source={content} />
+				</article>
+				<div className='divider-line'></div>
+				<div className='flex justify-start items-center gap-4 h-10 mt-6 text-gray-500'>
+					{serializationString(tags).map((tag, index) => (
+						<Link
+							href={`/search?keywords=${tag}`}
+							key={index}
+							className='px-2 py-1 bg-gray-300 rounded text-sm dark:bg-gray-900 dark:text-gray-300'
+						>
+							{tag}
+						</Link>
+					))}
+				</div>
+			</div>
+		</>
+	)
 }
 
 export const getStaticPaths = async () => {
-  const posts = await fetchPosts(1)
-  const paths = posts.items.map(post => ({
-    params: { param: [post.id.toString(), post.slug] },
-  }))
-  return {
-    paths,
-    fallback: 'blocking',
-  }
+	const posts = await fetchPosts(1)
+	const paths = posts.items.map(post => ({
+		params: { param: [post.id.toString(), post.slug] },
+	}))
+	return {
+		paths,
+		fallback: 'blocking',
+	}
 }
 
 export const getStaticProps = async ({ params: { param } }) => {
-  const [id, slug] = param
-  const post = await fetchPost(id)
-  return {
-    props: { post },
-    revalidate: 10,
-  }
+	const [id, slug] = param
+	const post = await fetchPost(id)
+	return {
+		props: { post },
+		revalidate: 10,
+	}
 }
 
 export default PostPage
